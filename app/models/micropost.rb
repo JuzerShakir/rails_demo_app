cc2318037b8d5f12ef133cc23698d21e671db409
JuzerShakir <juz.shakir.software@gmail.com>
2020-12-14 14:18:33 +0530

    Connect User and Micropost data

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,6 @@
 class Micropost < ActiveRecord::Base
+	# A micropost belongs to a user.
+	belongs_to :user
+	# Constraining microposts to be at most 140 characters.
+	validates :content, length: { maximum: 140 }
 end
